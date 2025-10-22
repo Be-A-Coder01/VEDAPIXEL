27a5ca9e653f0e9678be30cb2ce3d4d55556de28
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-10-22 11:43:03 +0530

    commit 38

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -145,11 +145,11 @@ const AboutPage = () => {
         >
           {/* --- Info Cards --- */}
           <div className="w-full   flex justify-end md:justify-end">
-            <div className="about-card  w-[32.5%] md:px-0 md:py-0 md:w-1/2 lg:w-1/3 h-[15vh] md:h-[40vh]">
+            <div className="about-card gap-3 md:gap-0 w-[32.5%] md:px-0 md:py-0 md:w-1/2 lg:w-1/3 h-[15vh] md:h-[40vh]">
               <p className="about-card-title text-[0.6rem] md:text-[1.75rem] text-[#F8F9FA]">
                 Our Mission
               </p>
-              <p className="about-card-title text-[0.4rem]  md:text-[1.5rem] text-[#C8C1C1]">
+              <p className="about-card-title text-[7px]  md:text-[1.5rem] text-[#C8C1C1]">
                 Vedapixel uses tech to bring clarity, speed, and reliability to
                 outdated sectors, building ecosystems where innovation can
                 thrive.
@@ -158,11 +158,11 @@ const AboutPage = () => {
           </div>
 
           <div className="w-full flex justify-center md:justify-center">
-            <div className="about-card border-2 border-green-600 w-[32.5%] md:px-0 md:py-0 md:w-1/2 lg:w-1/3 h-[15vh] md:h-[40vh]">
+            <div className="about-card gap-3 md:gap-0 border-2 border-green-600 w-[32.5%] md:px-0 md:py-0 md:w-1/2 lg:w-1/3 h-[15vh] md:h-[40vh]">
               <p className="about-card-title text-[0.6rem] md:text-[1.75rem] text-[#F8F9FA]">
                 Our Vision
               </p>
-              <p className="about-card-title text-[0.4rem]  md:text-[1.5rem] text-[#C8C1C1]">
+              <p className="about-card-title text-[7px]  md:text-[1.5rem] text-[#C8C1C1]">
                 We redefine how businesses use tech by creating integrated
                 platforms that simplify workflows and unlock new growth.
               </p>
@@ -170,11 +170,11 @@ const AboutPage = () => {
           </div>
 
           <div className="w-full flex justify-start md:justify-start">
-            <div className="about-card border-2 border-green-600 w-[32.5%] px-1 py-1 md:px-0 md:py-0 md:w-1/2 lg:w-1/3 h-[15vh] md:h-[40vh]">
+            <div className="about-card border-2 border-green-600 gap-3 md:gap-0 w-[32.5%] px-1 py-1 md:px-0 md:py-0 md:w-1/2 lg:w-1/3 h-[15vh] md:h-[40vh]">
               <p className="about-card-title text-[0.6rem] md:text-[1.75rem] text-[#F8F9FA]">
                 Our Story
               </p>
-              <p className="about-card-title text-[0.4rem]  md:text-[1.5rem] text-[#C8C1C1]">
+              <p className="about-card-title text-[7px]  md:text-[1.5rem] text-[#C8C1C1]">
                 Vedapixel Tech Solutions Pvt. Ltd. builds intelligent, scalable
                 digital platforms that solve real-world business challenges.
               </p>
